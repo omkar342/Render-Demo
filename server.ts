5d1c7fd2f7ebde5728256d14c2fa9c90b1652c42
omkar <omkarjadhav095@gmail.com>
2024-06-24 09:31:40 +0530

    changed message

--- server.ts
+++ server.ts
@@ -1,11 +1,12 @@
 import express from "express";
+require("dotenv").config();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello Render!");
+    res.send("Hello World!");
 })
 
 app.listen(PORT, () => {
